ffa8fd5994a5bc1f4bd4c86b167baa832a5c4ca9
kpflow369 <khushipatil187@gmail.com>
2026-04-10 12:57:08 +0530

    FIXED #5 issue

--- CForge/Views/ContentView.swift
+++ CForge/Views/ContentView.swift
@@ -1,24 +1,5 @@
 import SwiftUI
 
-extension Color {
-    static let darkBackground = Color(red: 0.08, green: 0.08, blue: 0.1)
-    static let darkerBackground = Color(red: 0.05, green: 0.05, blue: 0.07)
-    static let darkestBackground = Color(red: 0.03, green: 0.03, blue: 0.05)
-    static let neonBlue = Color(red: 0.0, green: 0.95, blue: 1.0)
-    static let neonPurple = Color(red: 0.3, green: 0.22, blue: 0.98)
-    static let neonGreen = Color(red: 0.3, green: 1.0, blue: 0.5)
-    static let neonPink = Color(red: 1.0, green: 0.2, blue: 0.7)
-    static let textPrimary = Color.white.opacity(0.9)
-    static let textSecondary = Color.white.opacity(0.6)
-    
-        
-}
-
-extension UIColor {
-    static let darkBackground = UIColor(red: 0.08, green: 0.08, blue: 0.1, alpha: 1.0)
-    static let darkerBackground = UIColor(red: 0.05, green: 0.05, blue: 0.07, alpha: 1.0)
-}
-
 struct ContentView: View {
     var body: some View {
         
